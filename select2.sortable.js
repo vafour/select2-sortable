9c2eb55d4ee326af637cd5e7136b88f881570118
Ayub <ayublin@gmail.com>
2013-07-08 16:42:11 +0700

    Callable ordering, destroy method, and direct init without select2 first.

--- select2.sortable.js
+++ select2.sortable.js
@@ -2,14 +2,14 @@
  * jQuery Select2 Sortable
  * - enable select2 to be sortable via normal select element
  * 
- * author      : Vafpress
+ * author      : Vafour
  * inspired by : jQuery Chosen Sortable (https://github.com/mrhenry/jquery-chosen-sortable)
  * License     : GPL
  */
 
 (function($){
 	$.fn.extend({
-		select2Order: function(){
+		select2SortableOrder: function(){
 			var $this      = this.filter('[multiple]').first(),
 			    $select2   = $this.siblings('.select2-container'),
 			    unselected = [],
@@ -28,39 +28,69 @@
 			}));
 
 			sorted.push.apply(sorted, unselected);
-			return sorted;
+
+			$this.children().remove();
+			$this.append(sorted);
+			$this.select2sortableDestroy();
+			$this.select2Sortable();
+
+			return $this;
 		},
-		select2Sortable: function(customSortableOptions){
-			var $this = this.filter('[multiple]');
+		select2Sortable: function(){
+			var args         = Array.prototype.slice.call(arguments, 0);
+			    $this        = this.filter('[multiple]'),
+			    validMethods = ['destroy'];
 
-            		var defaultSortableOptions = {
-				'placeholder' : 'ui-state-highlight',
-				'items'       : 'li:not(.select2-search-field)',
-				'tolerance'   : 'pointer'
-			};
-			
-			if(!customSortableOptions){
-				customSortableOptions = {};
+			if(args.length === 0 || typeof(args[0]) === 'object')
+			{
+				var defaultSortableOptions = {
+					'placeholder' : 'ui-state-highlight',
+					'items'       : 'li:not(.select2-search-field)',
+					'tolerance'   : 'pointer'
+				};
+
+				var sortableOptions = $.extend(defaultSortableOptions, args[0]);
+
+				$this.select2();
+
+				$this.each(function(){
+					var $select  = $(this);
+					var $select2 = $select.siblings('.select2-container');
+
+					// Init jQuery UI Sortable
+					$select2.find('.select2-choices').sortable(sortableOptions);
+
+					// Apply options ordering in form submit
+					$select.closest('form').unbind('submit.select2sortable').on('submit.select2sortable', function(){
+						$select.select2SortableOrder();
+					});
+				});
 			}
-			
-			var sortableOptions = $.extend(defaultSortableOptions, customSortableOptions);
-
+			else if(typeof(args[0] === 'string'))
+			{
+				if($.inArray(args[0], validMethods) == -1)
+				{
+					throw "Unknown method: " + args[0];
+				}
+				if(args[0] === 'destroy')
+				{
+					$this.select2sortableDestroy();
+				}
+			}
+			return $this;
+		},
+		select2sortableDestroy: function(){
+			var $this = this.filter('[multiple]');
 			$this.each(function(){
 				var $select  = $(this);
-				var $select2 = $select.siblings('.select2-container');
 
-				// Init jQuery UI Sortable
-				$select2.find('.select2-choices').sortable(sortableOptions);
+				// apply options ordering and unbind form submit event
+				$select.closest('form').unbind('submit.select2sortable');
 
-				// Apply options ordering in form submit
-				$select.closest('form').unbind('submit.select2sortable').on('submit.select2sortable', function(){
-					var $options = $select.select2Order();
-					$select.children().remove();
-					$select.append($options);
-					$select.select2("destroy");
-					$select.select2().select2Sortable();
-				});
+				// destroy select2
+				$this.select2('destroy');
 			});
+			return $this;
 		}
 	});
 }(jQuery));
